50261ccb5084dc1dee1574160e48715d6488656c
erick.valeriano <evalerianom@gmail.com>
2022-03-18 04:57:23 +0000

    Adding modals, colors and results

--- js/main.js
+++ js/main.js
@@ -1,88 +1,176 @@
 var options = ['rock','paper','scissors','lizard','spock']
 
-function showModal(){
-    console.log('HOAL')
-    toggleModal('modal');
+// Modal
+var modal = document.getElementById('defaultModal');
+console.log(modal)
+window.onclick = function(event) {
+    if (event.target == modal) {
+        toggleModal('defaultModal', false);
+
+    }
 }
 function myGame(valor){
     // shows the #modal element
     let scorePlayer = document.getElementById('score-player');
     let scorePc = document.getElementById('score-bot');
+    let showLoseWin = document.getElementById('showLoseWin')
+    let showPcOption = document.getElementById('showPcOption')
+
     let aleatorio = getRandom();
 
     let scorePlayerNumber = parseInt(scorePlayer.textContent);
     let scorePcNumber = parseInt(scorePc.textContent);
     
-
-    playerPc = options[aleatorio]
+    
+    let playerPc = options[aleatorio]
     switch(valor){
         case 'rock':
             if(playerPc==='scissors'){
-                console.log("Player win");
+                showLoseWin.textContent = "You Win";
+                showLoseWin.classList.remove('text-red-400');
+                showLoseWin.classList.remove('text-orange-300');
+                showLoseWin.classList.add('text-lime-500');
+                showPcOption.textContent = "Computer choose scissors";
                 scorePlayerNumber += 1;
             }else if(playerPc==='lizard'){
-                console.log("Player win");
+                showLoseWin.textContent = "You Win";
+                showLoseWin.classList.remove('text-red-400');
+                showLoseWin.classList.remove('text-orange-300');
+                showLoseWin.classList.add('text-lime-500');
+                showPcOption.textContent = "Computer choose lizard";
                 scorePlayerNumber += 1;
             }else if(playerPc==='rock'){
-                console.log("Draw")
+                showLoseWin.textContent = "It's a draw";
+                showLoseWin.classList.remove('text-red-400');
+                showLoseWin.classList.remove('text-lime-500');
+                showLoseWin.classList.add('text-orange-300');
+                showPcOption.textContent = "Computer choose rock";
             }else{
-                console.log("PC win")
+                showLoseWin.classList.remove('text-lime-500');
+                showLoseWin.classList.remove('text-orange-300');
+                showLoseWin.classList.add('text-red-400');
+                showLoseWin.textContent = "You Lost"
+                showPcOption.textContent = "Computer choose "+playerPc;
                 scorePcNumber += 1
             }
             break;
         case 'paper':
             if(playerPc==='rock'){
-                console.log("Player win");
+                showLoseWin.textContent = "You Win";
+                showLoseWin.classList.remove('text-red-400');
+                showLoseWin.classList.remove('text-orange-300');
+                showLoseWin.classList.add('text-lime-500');
+                showPcOption.textContent = "Computer choose rock";
                 scorePlayerNumber += 1;
             }else if(playerPc==='spock'){
-                console.log("Player win");
+                showLoseWin.textContent = "You Win";
+                showLoseWin.classList.remove('text-red-400');
+                showLoseWin.classList.remove('text-orange-300');
+                showLoseWin.classList.add('text-lime-500');
+                showPcOption.textContent = "Computer choose " + playerPc;
                 scorePlayerNumber += 1;
             }else if(playerPc==='paper'){
-                console.log("Draw")
+                showLoseWin.textContent = "It's a draw";
+                showLoseWin.classList.remove('text-red-400');
+                showLoseWin.classList.remove('text-lime-500');
+                showLoseWin.classList.add('text-orange-300');
+                showPcOption.textContent = "Computer choose paper";
             }else{
-                console.log("PC win");
+                showLoseWin.classList.remove('text-lime-500');
+                showLoseWin.classList.remove('text-orange-300');
+                showLoseWin.classList.add('text-red-400');
+                showLoseWin.textContent = "You Lost"
+                showPcOption.textContent = "Computer choose "+playerPc;
                 scorePcNumber += 1;
             }
             break;
         case 'scissors':
             if(playerPc==='paper'){
-                console.log("Player win");
+                showLoseWin.textContent = "You Win";
+                showLoseWin.classList.remove('text-red-400');
+                showLoseWin.classList.remove('text-orange-300');
+                showLoseWin.classList.add('text-lime-500');
+                showPcOption.textContent = "Computer choose " + playerPc;
                 scorePlayerNumber += 1;
             }else if(playerPc==='lizard'){
-                console.log("Player win");
+                showLoseWin.textContent = "You Win";
+                showLoseWin.classList.remove('text-red-400');
+                showLoseWin.classList.remove('text-orange-300');
+                showLoseWin.classList.add('text-lime-500');
+                showPcOption.textContent = "Computer choose " + playerPc;
                 scorePlayerNumber += 1;
             }else if(playerPc==='scissors'){
-                console.log("Draw")
+                showLoseWin.textContent = "It's a draw";
+                showLoseWin.classList.remove('text-red-400');
+                showLoseWin.classList.remove('text-lime-500');
+                showLoseWin.classList.add('text-orange-300');
+                showPcOption.textContent = "Computer choose " + playerPc;
             }else{
-                console.log("PC win");
+                showLoseWin.classList.remove('text-lime-500');
+                showLoseWin.classList.remove('text-orange-300');
+                showLoseWin.classList.add('text-red-400');
+                showLoseWin.textContent = "You Lost"
+                showPcOption.textContent = "Computer choose "+playerPc;
                 scorePcNumber += 1;
             }
             break;
         case 'lizard':
             if(playerPc==='paper'){
-                console.log("Player win");
+                showLoseWin.textContent = "You Win";
+                showLoseWin.classList.remove('text-red-400');
+                showLoseWin.classList.remove('text-orange-300');
+                showLoseWin.classList.add('text-lime-500');
+                showPcOption.textContent = "Computer choose " + playerPc;
                 scorePlayerNumber += 1;
             }else if(playerPc==='spock'){
-                console.log("Player win");
+                showLoseWin.textContent = "You Win";
+                showLoseWin.classList.remove('text-red-400');
+                showLoseWin.classList.remove('text-orange-300');
+                showLoseWin.classList.add('text-lime-500');
+                showPcOption.textContent = "Computer choose " + playerPc;
                 scorePlayerNumber += 1;
             }else if(playerPc==='lizard'){
-                console.log("Draw")
+                showLoseWin.textContent = "It's a draw";
+                showLoseWin.classList.remove('text-red-400');
+                showLoseWin.classList.remove('text-lime-500');
+                showLoseWin.classList.add('text-orange-300');
+                showPcOption.textContent = "Computer choose " + playerPc;
             }else{
-                console.log("PC win");
+                showLoseWin.classList.remove('text-lime-500');
+                showLoseWin.classList.remove('text-orange-300');
+                showLoseWin.classList.add('text-red-400');
+                showLoseWin.textContent = "You Lost";
+                showPcOption.textContent = "Computer choose "+playerPc;
                 scorePcNumber += 1;
             }
             break;
         case 'spock':
             if(playerPc==='scissors'){
-                console.log("Player win");
+                showLoseWin.textContent = "You Win";
+                showLoseWin.classList.remove('text-red-400');
+                showLoseWin.classList.remove('text-orange-300');
+                showLoseWin.classList.add('text-lime-500');
+                showPcOption.textContent = "Computer choose " + playerPc;
                 scorePlayerNumber += 1;
             }else if(playerPc==='rock'){
-                console.log("Player win");
+                showLoseWin.textContent = "You Win";
+                showLoseWin.classList.remove('text-red-400');
+                showLoseWin.classList.remove('text-orange-300');
+                showLoseWin.classList.add('text-lime-500');
+                showPcOption.textContent = "Computer choose " + playerPc;
                 scorePlayerNumber += 1;
             }else if(playerPc==='spock'){
-                console.log("Draw")
+                showLoseWin.textContent = "It's a draw";
+                showLoseWin.classList.remove('text-red-400');
+                showLoseWin.classList.remove('text-lime-500');
+                showLoseWin.classList.add('text-orange-300');
+                showPcOption.textContent = "Computer choose " + playerPc;
             }else{
-                console.log("PC win");
+                showLoseWin.classList.remove('text-lime-500');
+                showLoseWin.classList.remove('text-orange-300');
+                showLoseWin.classList.add('text-red-400');
+                showLoseWin.textContent = "You Lost";
+                showPcOption.textContent = "Computer choose "+playerPc;
                 scorePcNumber += 1;
             }
             break;
